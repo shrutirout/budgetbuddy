0f6624edf31075529bc510a5d67c5f970c54b007
Shruti Rout <shruti.rout@gmail.com>
2025-12-20 01:52:11 +0400

    Fix second API instance URL

--- budgetbuddy-frontend/src/api/axios.js
+++ budgetbuddy-frontend/src/api/axios.js
@@ -10,7 +10,7 @@ import axios from 'axios';
  */
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://budgetbuddy-backend-7qxx.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
